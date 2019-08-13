996f33b1d29c36f2148858fbb84abbe670478544
Krishna <krishna7902@gmail.com>
2019-08-13 18:41:30 -0400

    Done

--- SpringWithJersey/src/main/java/com/springwithjersey/oAuth2/AuthConstants.java
+++
@@ -1,9 +0,0 @@
-package com.springwithjersey.oAuth2;
-
-public final class AuthConstants {
-  private AuthConstants() {}
-
-  public static final String USER_PERMISSIONS_PARAM = "userPermissionsParam";
-  public static final String UNAUTHORIZED_ACCESS = "Unauthorized Access";
-
-}
--- SpringWithJersey/src/main/java/com/springwithjersey/rest/data/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springwithjersey.rest.data;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserRepository {
-
-
-}
